9d8b5d859e4ae735bdd1175d1d8a192f04f81892
buiquoctrieu0301 <61806789+buiquoctrieu0301@users.noreply.github.com>
2020-06-13 14:49:52 +1000

    Create sort.js

---
+++ sort.js
@@ -0,0 +1,12 @@
+function sortItem() {
+
+  var apples;
+  var bingo;
+  var jello;
+  var longVariableName = "Long text";
+  var markup = .3;
+}
+
+function createCharArray() {}
+
+function addNumbers() {}
